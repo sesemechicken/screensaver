6c7ff547602b95d17859361fe2c3bd7b4e193502
sesemechicken <jakeconti@hotmail.com>
2023-08-22 02:37:38 -0500

    added exe and resize

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ frametime = 0
 fireworkgroup = pg.sprite.Group()
 sparks = pg.sprite.Group()
 pygame.mouse.set_visible(False)
+sizemultiplyer = 2
 
 
 # define particals created by fireworks
@@ -32,56 +33,60 @@ class particle(pg.sprite.Sprite):
 
         # lifespan is how long the sparks are alive
         if type == "streamer":
-            self.height = 1.5
-            self.width = 2.5
+            self.height = 1.5 * sizemultiplyer
+            self.width = 2 * sizemultiplyer
             self.y = parent.rect.y + self.parent.height
+            self.image = pg.Surface((self.width, self.height))
+            # only for sparkle firework
+            self.image.fill("orange")
+
             if parent.type == "fountain":
-                self.image = pg.Surface((self.width, self.height))
+                self.image.fill("purple")
                 self.yacel = random.randrange(-5, -2)
                 self.xacel = random.randrange(-10, 10)
                 self.lifespan = 30
-                self.y = parent.rect.y+1
+                self.y = parent.rect.y + 1
 
             # for all firework moving trails
             elif parent.type != "explode":
-                self.image = pg.Surface((self.width, self.height))
                 self.yacel = random.randrange(2, 5)
                 self.xacel = random.randrange(-2, 2)
                 # fuselike sparks shorten over distance
                 self.lifespan = parent.yacel // 3 + ((parent.rect.y - parent.killspace) // 15)
 
-            # only for sparkle firework
+
             else:
-                self.image = pg.Surface((self.width, self.height))
                 self.yacel = random.randrange(10, 50) / 10
-                self.xacel = random.randrange(-5, 5) / 2
+                self.xacel = random.randrange(-50, 50) / 20
                 self.lifespan = 40
 
             # all sparks are orange
-            self.image.fill("orange")
+
         if type == "explode":
-            self.height = 3.5
-            self.width = 2
+            self.height = 3.5 * sizemultiplyer
+            self.width = 2.5 * sizemultiplyer
             self.image = pg.Surface((self.width, self.height))
             self.image.fill(self.parent.color)
-            self.yacel = random.randrange(-100, 100)
-            self.xacel = random.randrange(-100, 100)
+            self.yacel = random.randrange(-1000, 1000)
+            self.xacel = random.randrange(-1000, 1000)
             # lifespan is dependent on the type
-            self.lifespan = random.randrange(20, 100)
+            self.lifespan = random.randrange(200, 1000)
             if parent.type == "standard":
-                self.lifespan /= 1.9
-                self.xacel /= 2
-                self.yacel /= 2
+                self.lifespan /= 15
+                self.xacel /= 20
+                self.yacel /= 20
             elif parent.type == "extra":
-                self.lifespan *= 1.5
-            elif parent.type == "small":
-                self.lifespan /= 4
-                self.xacel /= 2.5
-                self.yacel /= 2.5
-            elif parent.type == "sparkle":
-                self.lifespan *= 1.5
+                self.lifespan /= 7
                 self.xacel /= 10
                 self.yacel /= 10
+            elif parent.type == "small":
+                self.lifespan /= 30
+                self.xacel /= 25
+                self.yacel /= 25
+            elif parent.type == "sparkle":
+                self.lifespan /= 7
+                self.xacel /= 100
+                self.yacel /= 100
         # shorter lifespan with higher speed
         self.lifespan -= (abs(self.yacel) + abs(self.xacel))
         self.rect = self.image.get_rect(center=(self.x, self.y))
@@ -110,41 +115,41 @@ class firework(pg.sprite.Sprite):
         super().__init__()
         self.type = type
         self.x = random.randrange(0, screen.get_width())
-        self.y = screen.get_height()-10
+        self.y = screen.get_height() - 10
         # killspace is the position to explode at unless fountain
         if type == "standard":
-            self.width = 8
-            self.height = 8
+            self.width = 8 * sizemultiplyer
+            self.height = 8 * sizemultiplyer
             self.color = "yellow"
             self.killspace = screen.get_height() // random.randrange(2, 5)
             self.yacel = random.randrange(5, 7)
             self.xacel = random.randrange(-1, 1)
         elif type == "extra":
-            self.width = 10
-            self.height = 10
+            self.width = 10 * sizemultiplyer
+            self.height = 10 * sizemultiplyer
             self.color = "blue"
             self.killspace = screen.get_height() // 6
             self.yacel = 10
             self.xacel = random.randrange(-1, 1)
         elif type == "fountain":
-            self.width = 10
-            self.height = 10
+            self.width = 10 * sizemultiplyer
+            self.height = 10 * sizemultiplyer
             self.x = random.randrange((screen.get_width() // 10), screen.get_width() - 100)
             self.color = "brown"
             self.timer = random.randrange(20, 100)
         elif type == "small":
-            self.width = 4
-            self.height = 4
+            self.width = 4 * sizemultiplyer
+            self.height = 4 * sizemultiplyer
             self.color = "red"
             self.killspace = screen.get_height() // 1.75
             self.yacel = random.randrange(3, 5)
             self.xacel = random.randrange(-1, 1)
         elif type == "sparkle":
-            self.width = 20
-            self.height = 20
+            self.width = 20 * sizemultiplyer
+            self.height = 20 * sizemultiplyer
             self.color = "white"
             self.killspace = screen.get_height() // 8
-            self.yacel = 1
+            self.yacel = 2
             self.xacel = 0
         # this keeps fireworks inbounds but sparkle doesn't move on x
         if self.type != "fountain" and self.type != "sparkle":
@@ -164,7 +169,7 @@ class firework(pg.sprite.Sprite):
             self.rect.x += self.xacel
             self.rect.y -= self.yacel
             if self.rect.y < self.killspace:
-                for _ in range(random.randrange(400, 600)):
+                for _ in range(random.randrange(100, 200)):
                     particle(self, "explode")
                 self.kill()
             for _ in range(random.randrange(1, 3)):
